810438ef30fb42666a711036f51ef4fc29f4c947
N0r3b0 <wfedde@gmail.com>
2025-04-23 15:38:05 +0200

    added tasklist json file creation and reading

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os, sys, argparse
 from termcolor import colored
+from task_manager import TaskManager
 
 parser = argparse.ArgumentParser(description="Program for managing your tasks")
 
-parser.add_argument("-l", "--list", action="store_true", help="name of the tasks list")
+parser.add_argument("-l", "--list", type=str, help="name of the tasks list")
+parser.add_argument("-c", "--create", type=str, help="create a new tasks list")
 parser.add_argument("-a", "--add", nargs='+', help="use -l to choose list, add new tasks into the list")
 parser.add_argument("-d", "--delete", nargs='+', help="use -l to choose list, delete tasks from the list")
 parser.add_argument("-u", "--update", action="store_true", help="use -l to choose list, update task on the list")
 parser.add_argument("-s", "--show", action="store_true", help="show all lists or 1 list if used with -l")
 parser.add_argument("-v", "--version", action="store_true", help="program version and repository location")
 args = parser.parse_args()
+
+task = TaskManager()
+
 
 if args.version:
     # 1.0.0: Init version
@@ -18,6 +23,15 @@ if args.version:
     print(f"{colored("Repo: ", "red")} https://{colored("github.com", "green")}/{colored("N0r3b0","cyan")}/{colored("PyTaskManager", "magenta")}")
     sys.exit(0)
 
+if args.create:
+    # create json file if doesnt exist
+    task.create_task_list(args.create)
+    pass
+
+if args.list:
+    # pull data from json
+    task.open_task_list(args.list)
+    pass
 
 if args.list and args.add:
     # dodaj zadania lub zadanie do listy
--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,32 @@
+import json
+from task import Task
 
 class TaskManager:
+    task_list: dict
+
+    def __init__(self):
+        pass
+
+
+
+    def create_task_list(self, file: str):
+        try:
+            with open(f"{file}.json", 'x') as f:  # 'x' creates file, raises FileExistsError if it exists
+                json.dump([], f)  
+        except FileExistsError:
+            print(f"The list {file} already exists")
+
+   
+    def open_task_list(self, file: str):
+        try:
+            with open(f"{file}.json", 'r') as f:
+                self.task_list = json.load(f)
+        except:
+            print(f"The list {file} does not exist")
+        
+        
+        
+        # print(self.task_list["skills"][2])
+
+
     
