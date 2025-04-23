bc6fa59c955d6fe2e1649e5e0f5225edad9fb225
N0r3b0 <wfedde@gmail.com>
2025-04-24 00:17:15 +0200

    added argument clear for clearing all task lists, added file to store names of the lists, fixed issue with empty list reading in open_list function

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ parser.add_argument("-c", "--create", type=str, help="create a new tasks list")
 parser.add_argument("-a", "--add", nargs='+', help="adds new task. Use -l to choose the list and than -a with 3 possible arguments (name required): name description status. Status options: true, yes, 1, done")
 parser.add_argument("-d", "--delete", type=str, help="deletes the task. Use -l to choose the list and than -a with an id of the task you want to remove")
 parser.add_argument("-u", "--update", action="store_true", help="use -l to choose list, update task on the list")
-parser.add_argument("-s", "--show", action="store_true", help="show all lists or 1 list if used with -l")
+parser.add_argument("-s", "--show", action="store_true", help="show available task lists or print tasks from the list if used with -l")
 parser.add_argument("-v", "--version", action="store_true", help="program version and repository location")
+
+parser.add_argument("--clear", action="store_true", help="clear all task lists")
+
 args = parser.parse_args()
 
 task = TaskManager()
+task.open_tasklists()
 
 
 if args.version:
@@ -23,9 +27,13 @@ if args.version:
     print(f"{colored("Repo: ", "red")} https://{colored("github.com", "green")}/{colored("N0r3b0","cyan")}/{colored("PyTaskManager", "magenta")}")
     sys.exit(0)
 
+if args.clear:
+    task.clear_tasklists()
+
 if args.create:
     # create json file if doesnt exist
     task.create_task_list(args.create)
+    task.open_tasklists()
     pass
 
 if args.list:
@@ -53,6 +61,5 @@ if args.list and args.update:
 
 if args.list and args.show:
     task.show_tasks()
-    pass
 if args.show:
-    pass
+    task.show_tasklists()
--- task_manager.py
+++ task_manager.py
@@ -1,14 +1,45 @@
 import json
+import os
 from task import Task
 
 class TaskManager:
     task_list: dict = {}
+    task_lists: dict = {}
+
+    def generate_id(self, dict: dict):
+        if not dict:
+            return "1"
+        return str(max(int(key) for key in dict.keys()) + 1)
+
+    # TASKLISTS
+    def save_tasklists(self, file: str):
+        id = self.generate_id(self.task_lists)
+        self.task_lists.update({id: {'task_list_name': file}})
+        with open("task_lists.json", 'w') as f:
+                json.dump(self.task_lists, f, indent=2)
+    
+    def open_tasklists(self):
+        if os.path.getsize("task_lists.json") == 0:
+            return  # Do nothing if the file is empty
+        with open("task_lists.json", 'r') as f:
+            self.task_lists = json.load(f)
+    
+    def clear_tasklists(self):
+        for id, name in self.task_lists.items():
+            if os.path.exists(f"{name['task_list_name']}.json"):
+                os.remove(f"{name['task_list_name']}.json")
+                print(f"{name['task_list_name']}.json has been removed")
+        with open("task_lists.json", 'w') as f:
+                print("All task lists has been cleared")
+        
 
 
+    # TASKLIST
     def create_task_list(self, file: str):
         try:
             with open(f"{file}.json", 'x') as f:  # 'x' creates file, raises FileExistsError if it exists
-                json.dump([], f)  
+                print(f"Task list {file} created") 
+                self.save_tasklists(file)
         except FileExistsError:
             print(f"The list {file} already exists")
 
@@ -16,15 +47,26 @@ class TaskManager:
     def open_task_list(self, file: str):
         try:
             with open(f"{file}.json", 'r') as f:
-                self.task_list = json.load(f)
-        except:
+                content = f.read().strip()
+                self.task_list = json.loads(content) if content else {}
+
+        except FileNotFoundError:
             print(f"The list {file} does not exist")
+            self.task_list = []
 
 
     def save_task_list(self, file: str):
         with open(f"{file}.json", 'w') as f:
             json.dump(self.task_list, f, indent=2)
+            
 
+    def show_tasklists(self):
+        print("📋 Task Lists:")
+        print("-" * 40)
+        for id, name in self.task_lists.items():
+            print(f"{id}. {name['task_list_name']}")
+
+    # TASK MANAGMENT
 
     def show_tasks(self):
         print("📋 Task List:")
@@ -38,25 +80,17 @@ class TaskManager:
             print("-" * 40)
 
     
-    # TASK MANAGMENT
-
-    def generate_task_id(self):
-        if not self.task_list:
-            return "1"
-        return str(max(int(key) for key in self.task_list.keys()) + 1)
-
-    
     def add_task(self, details):
         # details -> name, desc, status
         task = Task(details)
-        task_id = self.generate_task_id()
+        task_id = self.generate_id(self.task_list)
         self.task_list[task_id] = task.to_dict()
         print(f"Task added with ID {task_id}")
 
     def remove_task(self, id: str):
         try:
             self.task_list.pop(id)
-            print(f"Task with ID {id} removed")
+            print(f"Removed task with ID: {id} ")
         except:
             print("Wrong task id")
     
